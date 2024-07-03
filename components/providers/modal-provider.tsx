12dc1b260fb8d2a281680aefee59a4a6de11c881
Aufa Fauqi Ardhiqi <ardhiqi@gmail.com>
2024-07-04 04:19:12 +0700

    manage member server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import CreateServerModal from "@/components//modals/create-server-modal";
 import InviteModal from "@/components/modals/invite-modal";
 import EditServerModal from "@/components/modals/edit-server-modal";
+import MembersModal from "@/components/modals/members-modal";
 
 import React, { useEffect, useState } from "react";
 
@@ -19,6 +20,7 @@ const ModalProvider = () => {
       <CreateServerModal />
       <InviteModal/>
       <EditServerModal/>
+      <MembersModal/>
     </>
   );
 };
