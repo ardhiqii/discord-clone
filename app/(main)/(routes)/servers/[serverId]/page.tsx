e3457488b151573240b6421ce4600a706bea8a4e
Aufa Fauqi Ardhiqi <ardhiqi@gmail.com>
2024-08-28 10:21:42 +0700

    redirect to channel

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { RedirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const ServerIdPage = () => {
-  return (
-    <div>ServerIdPage</div>
-  )
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
 }
 
-export default ServerIdPage
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return <RedirectToSignIn />;
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      chanels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.chanels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(
+    `/servers/${params?.serverId}/channels/${initialChannel?.id}`
+  );
+};
+
+export default ServerIdPage;
